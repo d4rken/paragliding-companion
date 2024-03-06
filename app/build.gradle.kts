6779d9d85bac4b6d7b65c0c6ffd0f0a8da729640
darken <darken@darken.eu>
2024-03-06 13:35:59 +0100

    Fix "R8: Missing class java.time.DesugarLocalDate"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -138,7 +138,7 @@ android {
 }
 
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 
     addBase()
     addBaseUI()
